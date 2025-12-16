31fb836f672fe161743fef41cebdf1082cab7363
Marcelo Vaquilema <luis_vaquilema@hotmail.com>
2025-12-16 15:16:23 -0500

    Eliminacion de basura

---
+++ src/DescargaMasiva.Infrastructure/Adapters/Soap/Enumerations/QueryType.cs
@@ -0,0 +1,21 @@
+using Ardalis.SmartEnum;
+
+namespace DescargaMasiva.DescargaMasiva.Infrastructure.Adapters.Soap.Enumerations;
+
+public sealed class QueryType : SmartEnum<QueryType>
+{
+  /// <summary>
+  ///     0 = CFDI
+  /// </summary>
+  public static readonly QueryType Cfdi = new QueryType("CFDI", 0);
+
+  /// <summary>
+  ///     1 = Metadata
+  /// </summary>
+  public static readonly QueryType Metadata = new QueryType("Metadata", 1);
+
+  private QueryType(string name, int value) : base(name, value)
+  {
+  }
+
+}
